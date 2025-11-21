425b4747aea8b48da2b54bd3d6f699421985f023
lp63521 <00001132754161sp@al.educacao.sp.gov.br>
2025-11-21 20:01:56 -0300

    Update script.js

--- script.js
+++ script.js
@@ -10,3 +10,22 @@ window.addEventListener("scroll", () => {
 });
 
 // Menu mobile (implementar depois)
+
+// MENU MOBILE
+const toggle = document.querySelector(".menu-toggle");
+const nav = document.querySelector("nav");
+
+toggle.addEventListener("click", () => {
+    nav.classList.toggle("active");
+});
+
+// ANIMAÇÃO AO ROLAR
+window.addEventListener("scroll", () => {
+    const elements = document.querySelectorAll(".fade");
+    const trigger = window.innerHeight * 0.8;
+
+    elements.forEach(el => {
+        const top = el.getBoundingClientRect().top;
+        if (top < trigger) el.classList.add("show");
+    });
+});
